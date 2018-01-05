08db0976764bdbdc43b11558b4a8d7ecbe555949
unknown <vcianya>
2018-01-05 15:22:36 -0400

    first commit2

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const gt06 = require('./gt06n.js');
 const server = net.createServer((connection) => {
     connection.setEncoding('hex');
     connection.on('data', (data) => {
-        let result = gt06.parse(data);
-
-        console.log('vico');
+        console.log(data);
+        let result = gt06.parse(data);        
     });
 }).listen(5000);
 
